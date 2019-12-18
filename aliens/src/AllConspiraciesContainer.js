eec0cb9a039796b04ad6bdb1fc57af9998ea99a9
Kat M <mcLcomdata@protonmail.com>
2019-12-18 15:52:26 -0800

    initial setup

---
+++ aliens/src/AllConspiraciesContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class AllConspiraciesContainer extends Component {
+  render() {
+    return(
+      <h1>CONSPIRACIES</h1>
+    )
+  }
+}
+
+export default AllConspiraciesContainer
---
+++ aliens/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return(
+      <button type="button">ayyyy lmao</button>
+    )
+  }
+}
+
+export default App;
